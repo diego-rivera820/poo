388e9a419d81eda73891df8e90a62a46abe43e31
diego-rivera820 <diego.gomezriver@campusucc.edu.co>
2025-11-15 20:49:09 +0000

    termine las clases Direccion y Bonton

---
+++ Momento3/Boton.java
@@ -0,0 +1,58 @@
+package Momento3;
+
+public class Boton {
+
+    public abstract class tBoton {
+        private int piso;
+        private boolean iluminado;
+        private boolean falla;
+
+        public tBoton(int piso) {
+            this.piso = piso;
+            this.iluminado = false;
+            this.falla = false;
+        }
+
+        public int getpiso(){
+            return piso;
+        }
+
+        public boolean isiluminado() {
+            return iluminado;
+        }
+
+        public boolean isfalla(){
+            return falla;
+        }
+
+        public void fallar(){
+            falla = false;
+            System.out.println("El boton del piso " + piso + " ha fallado." );
+        }
+
+        public  void reparar(){
+            falla = true;
+            System.out.println("El boton del piso " + piso + " ha sido reparado." );
+        }
+
+        public void presionar(){
+            if (falla){
+                System.out.println("no se puede usar el boton del piso " + piso + " porque esta fallando. ");
+                return;
+            }
+            iluminado = true;
+            emitirSonido();
+            System.out.println("Boton para el  piso " + piso + " presionado - luz encendida.");
+        }
+
+        public void apagarLuz(){
+            iluminado = false;
+        }
+
+        protected void emitirSonido(){
+            System.out.println("beep suave: confirmacion de boton presionado.");
+        }
+
+    }
+    
+}
---
+++ Momento3/Direccion.java
@@ -0,0 +1,12 @@
+package Momento3;
+
+    public class Direccion {
+
+    public enum tDireccion {
+        SUBIR,
+        BAJAR,
+        INACTIVO
+        
+    }
+    
+}
